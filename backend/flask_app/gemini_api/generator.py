957473faf1ae395749b66865c6fd7678fdc42643
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-27 20:59:15 -0500

    00-removed old backend model

--- backend/flask_app/gemini_api/generator.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-# this class creates a generator that calls google gemini
-class Generator:
-
-    # constructor
-    def __init__(self):
-        genai.configure(api_key = "AIzaSyBxKLUxqvqlv87q67QodXN5GirOvXHtZ6k")
-        self.model = genai.GenerativeModel("gemini-1.5-flash")
